1ee163bb0c8d9d3061e8cbf6e51ff43ada84aa8a
David de Gelder <davidosodegelder@gmail.com>
2025-02-18 14:30:38 +0100

    Add Win feedback

--- Assets/Scripts/FillRenderer.cs
+++ Assets/Scripts/FillRenderer.cs
@@ -1,13 +1,31 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class FillRenderer : MonoBehaviour
 {
     public float scalePerLevel = 0.2f;
     public float lerpFactor= 0.1f;
+    public SpriteRenderer background;
+    public Color backgroundFilledColor;
+    
     private float scale;
+    private Color backgroundTargetColor;
+    
+    
     public void SetLevel(int level)
     {
         scale = Mathf.Lerp(scale, scalePerLevel * level, lerpFactor);
         transform.localScale = Vector3.one * scale;
+
+        if (level > 0)
+        {
+            backgroundTargetColor = backgroundFilledColor;
+        }
+        else
+        {
+            backgroundTargetColor = Color.white;
+        }
+
+        background.color = Color.Lerp(background.color, backgroundTargetColor, lerpFactor);
     }
 }
---
+++ Assets/Scripts/Puzzle.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Puzzle : MonoBehaviour
+{
+    private Hole[] holes;
+    private bool isComplete = false;
+    public Animator animator;
+
+    private void Start()
+    {
+        holes = GetComponentsInChildren<Hole>();
+    }
+
+    private void Update()
+    {
+        isComplete = true;
+        
+        foreach (var hole in holes)
+        {
+            if (hole.fillLevel == 0)
+            {
+                isComplete = false;
+                break;
+            }
+        }
+
+        if (isComplete)
+        {
+            animator.Play("Puzzle Complete");
+        }
+    }
+}
