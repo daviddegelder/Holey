e441e1cec02778f378a105d80a16f994ccedeb3e
David de Gelder <davidosodegelder@gmail.com>
2025-04-19 00:27:04 +0200

    Add Automatic Neighbour Detection

--- Hole/hole.gd
+++ Hole/hole.gd
@@ -1,39 +1,53 @@
+@tool
 class_name Hole
 extends Node2D
 
 enum Direction {
-		Null,
-		North,
-		NorthEast,
-		East,
-		SouthEast,
-		South,
-		SouthWest,
-		West,
-		NorthWest,
-	}
+	Null,
+	North,
+	NorthEast,
+	East,
+	SouthEast,
+	South,
+	SouthWest,
+	West,
+	NorthWest,
+}
+var DIRECTION_VECTORS: Dictionary[Direction, Vector2] = {
+	Direction.Null      : Vector2(0,0),
+	Direction.North     : Vector2(0,-1),
+	Direction.NorthEast : Vector2(1,-1),
+	Direction.East      : Vector2(1,0),
+	Direction.SouthEast : Vector2(1,1),
+	Direction.South     : Vector2(0,1),
+	Direction.SouthWest : Vector2(-1,1),
+	Direction.West      : Vector2(-1,0),
+	Direction.NorthWest : Vector2(-1,-1),
+}
 
+@export_tool_button("Detect Neighbours") var detect_neighbours_action = detect_neighbours;
 @export var neighbours: Dictionary[Direction, Hole]
 @export var fill_level: int
 
 var queue_fill: int = 0
 
 func _process(_delta):
-	var input_direction: Direction = get_input_direction()
-	if input_direction != Direction.Null:
-		flow(input_direction)
+	if not Engine.is_editor_hint():
+		var input_direction: Direction = get_input_direction()
+		if input_direction != Direction.Null:
+			flow(input_direction)
 	render_fill()
 	
 func flow(direction: Direction):
-	if fill_level > 0 and neighbours.has(direction):
+	if fill_level > 0 and has_neighbour(direction):
 		fill_level -= 1
-		neighbours.get(direction).fill.call_deferred()
+		neighbours[direction].fill.call_deferred()
 
 func fill():
 	fill_level += 1
 
 func render_fill():
-	%Fill.scale = Vector2.ONE * (fill_level * 0.3)
+	%Fill.scale = Vector2.ONE * (fill_level * 0.1)
 
 func get_input_direction():
 	if Input.is_action_just_released("North"):
@@ -54,3 +68,18 @@ func get_input_direction():
 		return Direction.NorthWest
 	#no input:
 	return Direction.Null
+
+func has_neighbour(direction: Direction):
+	return neighbours[direction] != null
+
+func detect_neighbours():
+	for direction in Direction.values():
+		neighbours[direction] = detect_neighbour_in_direction(direction)
+
+func detect_neighbour_in_direction(direction: Direction) -> Hole:
+	%RayCast2D.target_position = Global.HOLE_DISTANCE * DIRECTION_VECTORS[direction]
+	%RayCast2D.force_raycast_update()
+	var collider = %RayCast2D.get_collider()
+	if collider == null or !collider.is_in_group("Hole"):
+		return null
+	return collider.get_parent()
---
+++ global.gd
@@ -0,0 +1,3 @@
+extends Node
+
+const HOLE_DISTANCE: int = 64
